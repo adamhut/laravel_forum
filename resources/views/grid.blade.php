c99af09470193bffc2abcae55926b07e2de8f299
adamhut <adamhut@gmail.com>
2017-10-06 04:50:53 -0700

    test grid.blade.php

---
+++ resources/views/grid.blade.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style >
+    .container{
+        display: grid;
+        width: 500px;
+        height: 400px;
+        margin: 100px auto 0;
+
+    }
+    .item{
+        border: 1px solid #999;
+    }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="item">1</div>
+        <div class="item">2</div>
+        <div class="item">3</div>
+        <div class="item">4</div>
+        <div class="item">5</div>
+        <div class="item">6</div>
+        <div class="item">7</div>
+        <div class="item">8</div>
+    </div>
+
+
+</body>
+</html>
